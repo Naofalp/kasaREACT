8f4b94b4da7ba2104e2613d42ec6d958247fd1d7
NaoP <naop18@yahoo.fr>
2024-01-15 13:11:46 +0100

    création des routes principales

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,12 +1,26 @@
-import { createBrowserRouter } from "react-router-dom"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Header } from "./Header.jsx"
 
-const router = createBrowserRouter([
-  
-])
+
 
 function App() {
-
-
+  return (
+    <Router>
+      <div>
+        <Routes>
+          {/* Route page d'acceuil Home*/}
+          <Route path="/" element={<Home />} />
+          {/* Route page Apropos */}
+          <Route path="/Apropos" element={<Apropos />} />
+          {/* Route page Logements */}
+          <Route path="/Logement/:id" component={Logements} />
+          {/* Route page d'erreur Error */}
+          <Route path='/Error' element={<Error />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
