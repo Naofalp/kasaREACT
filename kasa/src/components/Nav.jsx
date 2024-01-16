cf2c3e7e36092708b93267d993d193fb929bd5e7
NaoP <naop18@yahoo.fr>
2024-01-16 12:13:45 +0100

    mise en forme

--- kasa/src/components/Nav.jsx
+++ kasa/src/components/Nav.jsx
@@ -1,13 +1,17 @@
 import { NavLink, Outlet } from "react-router-dom";
 
-export default function Header () {
+export default function Header() {
     return <>
         <nav>
-            <NavLink to ="/">Acceuil</NavLink>
-            <NavLink to ="/Apropos">A propos</NavLink>
+            <NavLink to="/">
+                Acceuil
+            </NavLink>
+            <NavLink to="/Apropos">
+                A propos
+            </NavLink>
         </nav>
-    <div>
-        <Outlet/>
-    </div>
+        <div>
+            <Outlet />
+        </div>
     </>
 }
