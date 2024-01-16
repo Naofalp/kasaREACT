7e6c2ab27c7bd3d61ccaf7c75050ccec0831cb7f
NaoP <naop18@yahoo.fr>
2024-01-16 12:12:24 +0100

    modif du nom Header en Nav, création du composants Nav

---
+++ kasa/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function Header () {
+    return <>
+        <nav>
+            <NavLink to ="/">Acceuil</NavLink>
+            <NavLink to ="/Apropos">A propos</NavLink>
+        </nav>
+    <div>
+        <Outlet/>
+    </div>
+    </>
+}
