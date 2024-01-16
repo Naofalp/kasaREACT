c1aeb1b31e7aad27df2059390cd52f07c79839a3
NaoP <naop18@yahoo.fr>
2024-01-16 14:01:52 +0100

    création+avancée class Header et Nav

--- kasa/src/components/Nav.jsx
+++ kasa/src/components/Nav.jsx
@@ -2,11 +2,11 @@ import { NavLink, Outlet } from "react-router-dom";
 
 export default function Nav() {
     return <>
-        <nav>
-            <NavLink to="/">
+        <nav className="nav-header">
+            <NavLink to="/" className="nav-headar_link">
                 Acceuil
             </NavLink>
-            <NavLink to="/Apropos">
+            <NavLink to="/Apropos" className="nav-headar_link">
                 A propos
             </NavLink>
         </nav>
