32af1b66f09ee8c35db407371cd2f9715e5b5a89
NaoP <naop18@yahoo.fr>
2024-01-18 18:05:36 +0100

    Ajouts import des pages dans le App.jsx

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./Pages/Home";
-import Footer from "./components/Footer"
-{/*
-import About 
-import Header 
-import FicheLogement 
-import Footer from 
-import Error from 
-*/}
+
+// import Apropos from "./Pages/Apropos.jsx";
+// import Logements from "./Pages/Logements.jsx"
+// import Error from "./Pages/Error.jsx";
+
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -21,7 +19,7 @@ function App() {
           {/* Route page d'acceuil Home*/}
           <Route path="/" element={<Home />} />
           {/* Route page Apropos */}
-          <Route path="/Apropos" element={<div>Apropos</div>} />
+          <Route path="/Apropos" element={<div>mettre fonction quand la page sera faite</div>} />
           {/* Route page Logements */}
           <Route path="/Logement/:id" element={<div>component=logement</div>} />
           {/* Route page d'erreur Error */}
