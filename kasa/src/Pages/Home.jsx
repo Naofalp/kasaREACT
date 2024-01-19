ef248481a3585892669a5d5311eade539cf4be9f
NaoP <naop18@yahoo.fr>
2024-01-19 22:03:47 +0100

    fetch des cards et infos

--- kasa/src/Pages/Home.jsx
+++ kasa/src/Pages/Home.jsx
@@ -1,13 +1,33 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import BannerHome from "../components/BannerHome";
 import CardLogements from "../components/CardLogements";
 
 
 export default function Home() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('/logements.json')
+            .then(response => { return response.json(); })
+            .then(data => { setData(data); }),
+            []
+    });
+
     return <>
         <BannerHome />
         <section className="card_container">
-            <CardLogements/>
+            {data.map((loc) => (
+                < CardLogements
+                    key={loc.id}
+                    lien={`/logement/${loc.id}`}
+                    cover={loc.cover}
+                    title={loc.title}
+                    />
+                    ))}
         </section>
     </>
 }
+
+// quand je garde que la fonction et je console.log (loc.quelquechos) ca apparait mais sinon ca renvoi un objet
--- kasa/src/components/CardLogements.jsx
+++ kasa/src/components/CardLogements.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import Home from "../Pages/Home";
 
-export default function CardLogements(lien, cover, title) {
+
+export default function CardLogements({lien, cover, title}) {
+
+// J'ai passé 1H30 a chercher pq "data" de home me renvoyait des objet au lieu de string : javais oublié
+// de mettre les {} dans les proprietes de la fonction...
+
+    return<>
     <Link to={lien} className="card_link">
         <article className="card_article">
-            <img src={cover} />
+            <img src={cover} alt={title}/>
             <p className="card_title">{title}</p>
         </article>
     </Link>
-
+    </>
 }
