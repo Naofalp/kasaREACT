0dde84be2dd66d7079fef88dc4388fc46509deaa
NaoP <naop18@yahoo.fr>
2024-02-08 17:13:29 +0100

    ajout responsive

--- kasa/src/components/BannerHome.jsx
+++ kasa/src/components/BannerHome.jsx
@@ -1,7 +1,7 @@
 export default function BannerHome () {
     return <>
         <div className="banner">
-                <h1 className="banner_title">Chez vous, partout et ailleurs</h1>
+                <h1 className="banner_title">Chez vous,<br className="mobile-break"/> partout et ailleurs</h1>
         </div>
     </>
 }
