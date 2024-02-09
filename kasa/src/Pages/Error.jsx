dcae7db1f802be27f1db54af85d561a02b78df8d
NaoP <naop18@yahoo.fr>
2024-02-09 17:34:11 +0100

    Responsive

--- kasa/src/Pages/Error.jsx
+++ kasa/src/Pages/Error.jsx
@@ -4,7 +4,7 @@ export default function Error() {
     return <>
         <div className="error_container">
             <p className="error_number">404</p>
-            <p className="error_text">Oups! La page que vous demandez n'existe pas.</p>
+            <p className="error_text">Oups! La page que <br className="mobile-break"/> vous demandez n'existe pas.</p>
             <NavLink to="/" className="error_link">
                 Retourner sur la page d’accueil
             </NavLink>
