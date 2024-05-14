7d94a797b40450a8db5eb69e53b2434c0005577d
NaoP <naop18@yahoo.fr>
2024-05-14 23:38:25 +0200

    browserrouter en hashrouter

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./Pages/Home";
 import Apropos from "./Pages/Apropos.jsx";
